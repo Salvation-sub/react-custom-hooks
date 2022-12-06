03e6bea681f677868617c7a20a5ba4769a22709b
Salvation_sub <kyman19940214@gmail.com>
2022-12-06 15:37:06 +0900

    custom hooks

---
+++ README.md
@@ -0,0 +1,16 @@
+# hooks
+
+- [] useTitle
+- [x] useInput
+- [] usePageLeave
+- [] useClick
+- [] useFadeIn
+- [] useFullScreen
+- [] useHover
+- [] useNetwork
+- [] useNotification
+- [] useScroll
+- [x] useTabs
+- [] usePreventLeave
+- [] useConfirm
+- [] useAxios
---
+++ useInput/useInput.js
@@ -0,0 +1,17 @@
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    // 블리언 함수를 받아서 input에 조건을 넣는다.
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
